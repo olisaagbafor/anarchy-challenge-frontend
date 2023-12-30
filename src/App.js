69bf0bf361169622bbd44241ddc78d38e6e96558
Ishmael Destiny O. <134241711+codewithdestiny@users.noreply.github.com>
2023-12-30 06:49:32 +0100

    added sign up and login page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import {Helmet} from 'react-helmet';
 import {Routes, Route} from 'react-router-dom';
 import Error404 from './pages/Error404';
 import Home from './pages/Home';
+import Login from './pages/Login';
 import DefaultLayout from './components/DefaultLayout';
+import AuthPage from './pages/AuthPage';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
@@ -19,6 +22,10 @@ function App() {
       <Route element={<DefaultLayout/>}>
         <Route  index element={<Home/>}/>
       </Route>
+        <Route path='/auth/login' element={<AuthPage/>}/>
+        <Route path='/auth/sign-up' element={<SignUp/>}/>
+        <Route path='/auth/login/identifier' element={<Login/>}/>
+      <Route path='/*' element={<Error404/>}/>
      </Routes>
       
     </div>
